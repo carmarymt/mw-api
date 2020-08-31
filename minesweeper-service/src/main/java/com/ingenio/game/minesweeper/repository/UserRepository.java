b4354a514f88e5737b7cc8d226628143e2256c4f
carmary.mendez <carmarymt@gmail.com>
2020-08-31 08:30:44 -0300

    Fix import userRepository, userService

--- minesweeper-service/src/main/java/com/ingenio/game/minesweeper/repository/UserRepository.java
+++ minesweeper-service/src/main/java/com/ingenio/game/minesweeper/repository/UserRepository.java
@@ -2,10 +2,7 @@ package com.ingenio.game.minesweeper.repository;
 
 import com.ingenio.game.minesweeper.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.stream.Stream;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
--- minesweeper-service/src/main/java/com/ingenio/game/minesweeper/service/UserService.java
+++ minesweeper-service/src/main/java/com/ingenio/game/minesweeper/service/UserService.java
@@ -8,7 +8,6 @@ import com.ingenio.game.minesweeper.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Instant;
